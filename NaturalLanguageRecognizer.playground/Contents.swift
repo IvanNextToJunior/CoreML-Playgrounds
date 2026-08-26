390ef87796b576422fbceaf497c05d1295df2cb0
IvanNextToJunior <Vanwan@bk.ru>
2026-08-26 20:53:49 +0300

    add text for recognition

--- NaturalLanguageRecognizer.playground/Contents.swift
+++ NaturalLanguageRecognizer.playground/Contents.swift
@@ -1,2 +1,9 @@
 import NaturalLanguage
-
+import Foundation
+let text = "Пусть это упражнение станет первым шагом на пути к тому, чтобы мы получили ИИ-сборщика продуктов"
+if let language = NLLanguageRecognizer.dominantLanguage(for: text) {
+    print("Detected \(language.rawValue.uppercased()) as dominant language for \n\"\(text)\"")
+}
+else {
+    print("Couldn't recognize language for \(text)")
+}
