42dbba8a7a5f217ee00faadc5306fdf183e51b0c
IvanNextToJunior <Vanwan@bk.ru>
2026-08-27 19:34:13 +0300

    add new playground

---
+++ Semantic Units.playground/Contents.swift
@@ -0,0 +1,13 @@
+import NaturalLanguage
+
+let text = "Steve Jobs, Steve Wozniak, and Ronald Wayne founded Apple Computer in the garage of Steve Jobs's Los Altos home."
+
+let tagger = NLTagger(tagSchemes: [NLTagScheme.nameTypeOrLexicalClass])
+
+tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: NLTokenUnit.word, scheme: NLTagScheme.nameTypeOrLexicalClass, options: [.omitPunctuation, .omitWhitespace, NLTagger.Options.joinNames]){ (tag, range) -> Bool in
+    
+    print(text[range])
+    print(tag?.rawValue ?? "unknown")
+    return true
+    
+}
