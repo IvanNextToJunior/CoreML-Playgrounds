86d29ff718fab73ac2c59816297333553b55757d
IvanNextToJunior <Vanwan@bk.ru>
2026-08-27 18:19:01 +0300

    Update Contents.swift

--- String_Tokenizer.playground/Contents.swift
+++ String_Tokenizer.playground/Contents.swift
@@ -1,3 +1,9 @@
 import NaturalLanguage
-let text = "Knowledge will give you power, but character respect."
 
+let text = "Knowledge will give you power, but character respect."
+let tagger = NLTagger(tagSchemes: [NLTagScheme.tokenType])
+tagger.string = text
+tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: NLTokenUnit.word, scheme: NLTagScheme.tokenType, options: [.omitPunctuation, .omitWhitespace]) {(tag, rannge) -> Bool in
+    print(text[rannge])
+    return true
+}
