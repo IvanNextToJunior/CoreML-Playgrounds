bb8cf8d277358de4131732f2bb55d8a0b22f8439
IvanNextToJunior <Vanwan@bk.ru>
2026-09-01 21:47:11 +0300

    create project; add camera button

---
+++ PhotoAnalyzer/PhotoAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAnalyzer
+//
+//  Created by Галина Маслова on 01.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
