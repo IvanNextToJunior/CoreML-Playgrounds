ea6b8c378a5d35728fc3d1759f3d834362bb111a
IvanNextToJunior <Vanwan@bk.ru>
2026-09-02 15:25:38 +0300

    implement image picker

--- PhotoAnalyzer/PhotoAnalyzer/ViewController.swift
+++ PhotoAnalyzer/PhotoAnalyzer/ViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {presentPhotoPicker(sourceType: .photoLibrary)
+        return}
+        let photoSourcePicker = UIAlertController()
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default){[weak self] _ in
+            self?.presentPhotoPicker(sourceType: .camera)
+        }
+        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) {[weak self] _ in self?.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        photoSourcePicker.addAction(takePhoto)
+        photoSourcePicker.addAction(choosePhoto)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(photoSourcePicker, animated: true)
+    }
+    
+    private func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +44,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {fatalError("Couldn't retrieve image from picker")}
+        
+        imageView.image = image
+        
+    }
+}
